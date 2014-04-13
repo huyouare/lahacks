d046b168ca5d65326d802971d21205dee6081cb7
Jesse Hu <huyouare@gmail.com>
2014-04-13 01:43:48 -0400

    fixed issues with sliding, added jquery

--- js/page-slide.js
+++ js/page-slide.js
@@ -1,23 +1,41 @@
-var currPage = ".page1";
+$(document).ready(function(){
+  var currPage = ".page1";
+  $('.page2').hide();
+  $('.page3').hide();
 
-
-$(function () {
-    $('.toggle1').on("click", function (e) {
+  $(function () {
+      $('.toggle1').on("click", function (e) {
+          console.log("cl");
+          e.preventDefault();
+          if(currPage!=".page1"){
+            $(currPage).hide("slide", {direction: "right"}, "fast");
+            $('.page1').delay(100).fadeIn();
+            currPage = ".page1";
+            $('.active').removeClass('active');
+            $('.li1').addClass('active');
+          }
+      });
+      $('.toggle2').on("click", function (e) {
         console.log("cl");
         e.preventDefault();
-        if(currPage!=".page1"){
-          $(currPage).hide();
-          $('.page1').show();
-          currPage = ".page1";
-        }
-    });
-    $('.toggle2').on("click", function (e) {
-      console.log("cl");
-        e.preventDefault();
         if(currPage!=".page2"){
-          $(currPage).hide();
-          $('.page2').show();
+          $(currPage).hide("slide", {direction: "right"}, "fast");
+          $('.page2').delay(100).fadeIn();
           currPage = ".page2";
+          $('.active').removeClass('active');
+          $('.li2').addClass('active');
         }
-    });
+      });
+      $('.toggle3').on("click", function (e) {
+        console.log("cl");
+          e.preventDefault();
+          if(currPage!=".page3"){
+            $(currPage).hide("slide", {direction: "right"}, "fast");
+            $('.page3').delay(100).fadeIn();
+            currPage = ".page3";
+            $('.active').removeClass('active');
+            $('.li3').addClass('active');
+          }
+      });
+  });
 });
